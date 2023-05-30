ec4bf4d3ba4f01911e819f052730ed388575cb79
fireonthesun <56504746+fireonthesun@users.noreply.github.com>
2023-05-30 20:47:27 +0100

    Formatting

--- src/App.js
+++ src/App.js
@@ -7,55 +7,55 @@ import { FaGlobeAmericas } from 'react-icons/fa';
 function App() {
   return (
     <body>
-    <div className="App">
-      <header className="App-header">
-        <FaGlobeAmericas className="App-logo" />
-        
-        
-        <h1 id = "header" >Where do you want to go?</h1>
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <FaGlobeAmericas className="App-logo" />
+
+
+          <h1 id="header" >Where do you want to go?</h1>
+        </header>
 
         <div className='container'>
 
-        <img src="https://shorturl.at/orE58" width={450} height={250} alt="Paris" />
-        <img src="https://shorturl.at/bxEJQ" width={450} height={250} alt="Milan" />
-        <img src="https://shorturl.at/stBM8" width={450} height={250} alt="Barcelona"/>       
-        
+          <img src="https://shorturl.at/orE58" width={450} height={250} alt="Paris" />
+          <img src="https://shorturl.at/bxEJQ" width={450} height={250} alt="Milan" />
+          <img src="https://shorturl.at/stBM8" width={450} height={250} alt="Barcelona" />
+
         </div>
 
         <div className="buttons">
-              
-        <a
-        className="App-link"
-        href="Paris.js"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Button message='Paris' />
-      </a>
 
-<a
-          
-className="App-link"
-href="Milan.js"
-target="_blank"
-rel="noopener noreferrer"
->
-<Button message='Milan' />
-</a>
+          <a
+            className="App-link"
+            href="Paris.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button message='Paris' />
+          </a>
 
-<a
-          
-className="App-link"
-href="Barcelona.js"
-target="_blank"
-rel="noopener noreferrer"
->
-<Button message='Barcelona' />
-</a>
+          <a
+
+            className="App-link"
+            href="Milan.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button message='Milan' />
+          </a>
+
+          <a
+
+            className="App-link"
+            href="Barcelona.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button message='Barcelona' />
+          </a>
         </div>
 
-    </div>
+      </div>
     </body>
   );
 }
