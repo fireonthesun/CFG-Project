053c53a5247b567eb3619b5c9647b47f723e0e70
dne15 <d.memie@gmail.com>
2023-05-30 19:51:01 +0100

    changes 2

--- src/App.js
+++ src/App.js
@@ -6,45 +6,57 @@ import { FaGlobeAmericas } from 'react-icons/fa';
 
 function App() {
   return (
-    
+    <body>
     <div className="App">
       <header className="App-header">
         <FaGlobeAmericas className="App-logo" />
-       
-        <h1 id = "header" >Where do you want to go?</h1>
-  
-        <p>
-        </p>
-        <span><img src="https://shorturl.at/orE58" width={450} height={250} alt="Paris" />
-        </span>
-                <a
         
-          className="App-link"
-          href="Paris.js"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button message='Paris' />
-        </a>
-
-        <span>
-          <img src="https://shorturl.at/bxEJQ" width={450} height={250} alt="Milan" />
-        </span>
-
-        {/* <a
-          
-          className="App-link2"
-          href="Milan.js"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button2 message='Milan' /> */}
-
-{/* 
-        </a>
-         */}
+        
+        <h1 id = "header" >Where do you want to go?</h1>
       </header>
+
+        <div className='container'>
+
+        <img src="https://shorturl.at/orE58" width={450} height={250} alt="Paris" />
+        <img src="https://shorturl.at/bxEJQ" width={450} height={250} alt="Milan" />
+        <img src="https://shorturl.at/stBM8" width={450} height={250} alt="Barcelona"/>       
+        
+        </div>
+
+        <div className="buttons">
+              
+        <a
+        className="App-link"
+        href="Paris.js"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button message='Paris' />
+      </a>
+
+<a
+          
+className="App-link"
+href="Milan.js"
+target="_blank"
+rel="noopener noreferrer"
+>
+<Button message='Milan' />
+</a>
+
+<a
+          
+className="App-link"
+href="Barcelona.js"
+target="_blank"
+rel="noopener noreferrer"
+>
+<Button message='Barcelona' />
+</a>
+        </div>
+
     </div>
+    </body>
   );
 }
 
